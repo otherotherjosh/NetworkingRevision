5f9b2acf78ce4623e6ceb4ea248e8a287d971a4f
otherotherjosh <itsotherjosh@gmail.com>
2023-10-25 13:43:27 +1300

    added buttons for all modules

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -33,6 +33,25 @@
             this.moduCheck1 = new System.Windows.Forms.CheckBox();
             this.moduCheck2 = new System.Windows.Forms.CheckBox();
             this.moduCheck3 = new System.Windows.Forms.CheckBox();
+            this.moduCheck6 = new System.Windows.Forms.CheckBox();
+            this.moduCheck5 = new System.Windows.Forms.CheckBox();
+            this.moduCheck4 = new System.Windows.Forms.CheckBox();
+            this.moduCheck4_7 = new System.Windows.Forms.CheckBox();
+            this.moduCheck10 = new System.Windows.Forms.CheckBox();
+            this.moduCheck9 = new System.Windows.Forms.CheckBox();
+            this.moduCheck8 = new System.Windows.Forms.CheckBox();
+            this.moduCheck8_10 = new System.Windows.Forms.CheckBox();
+            this.moduCheck13 = new System.Windows.Forms.CheckBox();
+            this.moduCheck12 = new System.Windows.Forms.CheckBox();
+            this.moduCheck11 = new System.Windows.Forms.CheckBox();
+            this.moduCheck11_13 = new System.Windows.Forms.CheckBox();
+            this.moduCheck15 = new System.Windows.Forms.CheckBox();
+            this.moduCheck14 = new System.Windows.Forms.CheckBox();
+            this.moduCheck14_15 = new System.Windows.Forms.CheckBox();
+            this.moduCheck7 = new System.Windows.Forms.CheckBox();
+            this.moduCheck17 = new System.Windows.Forms.CheckBox();
+            this.moduCheck16 = new System.Windows.Forms.CheckBox();
+            this.moduCheck16_17 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // startButton
@@ -56,7 +75,7 @@
             this.moduCheck1_3.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.moduCheck1_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.moduCheck1_3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.moduCheck1_3.Location = new System.Drawing.Point(616, 203);
+            this.moduCheck1_3.Location = new System.Drawing.Point(478, 163);
             this.moduCheck1_3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
             this.moduCheck1_3.Name = "moduCheck1_3";
             this.moduCheck1_3.Size = new System.Drawing.Size(95, 37);
@@ -69,7 +88,7 @@
             // moduCheck1
             // 
             this.moduCheck1.AutoSize = true;
-            this.moduCheck1.Location = new System.Drawing.Point(616, 250);
+            this.moduCheck1.Location = new System.Drawing.Point(478, 210);
             this.moduCheck1.Name = "moduCheck1";
             this.moduCheck1.Size = new System.Drawing.Size(84, 20);
             this.moduCheck1.TabIndex = 3;
@@ -80,7 +99,7 @@
             // moduCheck2
             // 
             this.moduCheck2.AutoSize = true;
-            this.moduCheck2.Location = new System.Drawing.Point(616, 276);
+            this.moduCheck2.Location = new System.Drawing.Point(478, 236);
             this.moduCheck2.Name = "moduCheck2";
             this.moduCheck2.Size = new System.Drawing.Size(84, 20);
             this.moduCheck2.TabIndex = 4;
@@ -91,7 +110,7 @@
             // moduCheck3
             // 
             this.moduCheck3.AutoSize = true;
-            this.moduCheck3.Location = new System.Drawing.Point(616, 302);
+            this.moduCheck3.Location = new System.Drawing.Point(478, 262);
             this.moduCheck3.Name = "moduCheck3";
             this.moduCheck3.Size = new System.Drawing.Size(84, 20);
             this.moduCheck3.TabIndex = 5;
@@ -99,11 +118,250 @@
             this.moduCheck3.UseVisualStyleBackColor = true;
             this.moduCheck3.Click += new System.EventHandler(this.moduCheck3_Click);
             // 
+            // moduCheck6
+            // 
+            this.moduCheck6.AutoSize = true;
+            this.moduCheck6.Location = new System.Drawing.Point(630, 262);
+            this.moduCheck6.Name = "moduCheck6";
+            this.moduCheck6.Size = new System.Drawing.Size(84, 20);
+            this.moduCheck6.TabIndex = 9;
+            this.moduCheck6.Text = "Module 6";
+            this.moduCheck6.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck5
+            // 
+            this.moduCheck5.AutoSize = true;
+            this.moduCheck5.Location = new System.Drawing.Point(630, 236);
+            this.moduCheck5.Name = "moduCheck5";
+            this.moduCheck5.Size = new System.Drawing.Size(84, 20);
+            this.moduCheck5.TabIndex = 8;
+            this.moduCheck5.Text = "Module 5";
+            this.moduCheck5.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck4
+            // 
+            this.moduCheck4.AutoSize = true;
+            this.moduCheck4.Location = new System.Drawing.Point(630, 210);
+            this.moduCheck4.Name = "moduCheck4";
+            this.moduCheck4.Size = new System.Drawing.Size(84, 20);
+            this.moduCheck4.TabIndex = 7;
+            this.moduCheck4.Text = "Module 4";
+            this.moduCheck4.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck4_7
+            // 
+            this.moduCheck4_7.AutoSize = true;
+            this.moduCheck4_7.BackColor = System.Drawing.SystemColors.Control;
+            this.moduCheck4_7.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.moduCheck4_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moduCheck4_7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.moduCheck4_7.Location = new System.Drawing.Point(630, 163);
+            this.moduCheck4_7.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
+            this.moduCheck4_7.Name = "moduCheck4_7";
+            this.moduCheck4_7.Size = new System.Drawing.Size(95, 37);
+            this.moduCheck4_7.TabIndex = 6;
+            this.moduCheck4_7.TabStop = false;
+            this.moduCheck4_7.Text = "Modules 4-7";
+            this.moduCheck4_7.UseVisualStyleBackColor = false;
+            // 
+            // moduCheck10
+            // 
+            this.moduCheck10.AutoSize = true;
+            this.moduCheck10.Location = new System.Drawing.Point(782, 262);
+            this.moduCheck10.Name = "moduCheck10";
+            this.moduCheck10.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck10.TabIndex = 13;
+            this.moduCheck10.Text = "Module 10";
+            this.moduCheck10.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck9
+            // 
+            this.moduCheck9.AutoSize = true;
+            this.moduCheck9.Location = new System.Drawing.Point(782, 236);
+            this.moduCheck9.Name = "moduCheck9";
+            this.moduCheck9.Size = new System.Drawing.Size(84, 20);
+            this.moduCheck9.TabIndex = 12;
+            this.moduCheck9.Text = "Module 9";
+            this.moduCheck9.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck8
+            // 
+            this.moduCheck8.AutoSize = true;
+            this.moduCheck8.Location = new System.Drawing.Point(782, 210);
+            this.moduCheck8.Name = "moduCheck8";
+            this.moduCheck8.Size = new System.Drawing.Size(84, 20);
+            this.moduCheck8.TabIndex = 11;
+            this.moduCheck8.Text = "Module 8";
+            this.moduCheck8.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck8_10
+            // 
+            this.moduCheck8_10.AutoSize = true;
+            this.moduCheck8_10.BackColor = System.Drawing.SystemColors.Control;
+            this.moduCheck8_10.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.moduCheck8_10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moduCheck8_10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.moduCheck8_10.Location = new System.Drawing.Point(782, 163);
+            this.moduCheck8_10.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
+            this.moduCheck8_10.Name = "moduCheck8_10";
+            this.moduCheck8_10.Size = new System.Drawing.Size(103, 37);
+            this.moduCheck8_10.TabIndex = 10;
+            this.moduCheck8_10.TabStop = false;
+            this.moduCheck8_10.Text = "Modules 8-10";
+            this.moduCheck8_10.UseVisualStyleBackColor = false;
+            // 
+            // moduCheck13
+            // 
+            this.moduCheck13.AutoSize = true;
+            this.moduCheck13.Location = new System.Drawing.Point(478, 439);
+            this.moduCheck13.Name = "moduCheck13";
+            this.moduCheck13.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck13.TabIndex = 17;
+            this.moduCheck13.Text = "Module 13";
+            this.moduCheck13.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck12
+            // 
+            this.moduCheck12.AutoSize = true;
+            this.moduCheck12.Location = new System.Drawing.Point(478, 413);
+            this.moduCheck12.Name = "moduCheck12";
+            this.moduCheck12.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck12.TabIndex = 16;
+            this.moduCheck12.Text = "Module 12";
+            this.moduCheck12.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck11
+            // 
+            this.moduCheck11.AutoSize = true;
+            this.moduCheck11.Location = new System.Drawing.Point(478, 387);
+            this.moduCheck11.Name = "moduCheck11";
+            this.moduCheck11.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck11.TabIndex = 15;
+            this.moduCheck11.Text = "Module 11";
+            this.moduCheck11.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck11_13
+            // 
+            this.moduCheck11_13.AutoSize = true;
+            this.moduCheck11_13.BackColor = System.Drawing.SystemColors.Control;
+            this.moduCheck11_13.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.moduCheck11_13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moduCheck11_13.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.moduCheck11_13.Location = new System.Drawing.Point(478, 340);
+            this.moduCheck11_13.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
+            this.moduCheck11_13.Name = "moduCheck11_13";
+            this.moduCheck11_13.Size = new System.Drawing.Size(111, 37);
+            this.moduCheck11_13.TabIndex = 14;
+            this.moduCheck11_13.TabStop = false;
+            this.moduCheck11_13.Text = "Modules 11-13";
+            this.moduCheck11_13.UseVisualStyleBackColor = false;
+            // 
+            // moduCheck15
+            // 
+            this.moduCheck15.AutoSize = true;
+            this.moduCheck15.Location = new System.Drawing.Point(630, 413);
+            this.moduCheck15.Name = "moduCheck15";
+            this.moduCheck15.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck15.TabIndex = 20;
+            this.moduCheck15.Text = "Module 15";
+            this.moduCheck15.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck14
+            // 
+            this.moduCheck14.AutoSize = true;
+            this.moduCheck14.Location = new System.Drawing.Point(630, 387);
+            this.moduCheck14.Name = "moduCheck14";
+            this.moduCheck14.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck14.TabIndex = 19;
+            this.moduCheck14.Text = "Module 14";
+            this.moduCheck14.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck14_15
+            // 
+            this.moduCheck14_15.AutoSize = true;
+            this.moduCheck14_15.BackColor = System.Drawing.SystemColors.Control;
+            this.moduCheck14_15.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.moduCheck14_15.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moduCheck14_15.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.moduCheck14_15.Location = new System.Drawing.Point(630, 340);
+            this.moduCheck14_15.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
+            this.moduCheck14_15.Name = "moduCheck14_15";
+            this.moduCheck14_15.Size = new System.Drawing.Size(111, 37);
+            this.moduCheck14_15.TabIndex = 18;
+            this.moduCheck14_15.TabStop = false;
+            this.moduCheck14_15.Text = "Modules 14-15";
+            this.moduCheck14_15.UseVisualStyleBackColor = false;
+            // 
+            // moduCheck7
+            // 
+            this.moduCheck7.AutoSize = true;
+            this.moduCheck7.Location = new System.Drawing.Point(630, 288);
+            this.moduCheck7.Name = "moduCheck7";
+            this.moduCheck7.Size = new System.Drawing.Size(84, 20);
+            this.moduCheck7.TabIndex = 22;
+            this.moduCheck7.Text = "Module 7";
+            this.moduCheck7.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck17
+            // 
+            this.moduCheck17.AutoSize = true;
+            this.moduCheck17.Location = new System.Drawing.Point(782, 413);
+            this.moduCheck17.Name = "moduCheck17";
+            this.moduCheck17.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck17.TabIndex = 25;
+            this.moduCheck17.Text = "Module 17";
+            this.moduCheck17.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck16
+            // 
+            this.moduCheck16.AutoSize = true;
+            this.moduCheck16.Location = new System.Drawing.Point(782, 387);
+            this.moduCheck16.Name = "moduCheck16";
+            this.moduCheck16.Size = new System.Drawing.Size(91, 20);
+            this.moduCheck16.TabIndex = 24;
+            this.moduCheck16.Text = "Module 16";
+            this.moduCheck16.UseVisualStyleBackColor = true;
+            // 
+            // moduCheck16_17
+            // 
+            this.moduCheck16_17.AutoSize = true;
+            this.moduCheck16_17.BackColor = System.Drawing.SystemColors.Control;
+            this.moduCheck16_17.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.moduCheck16_17.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moduCheck16_17.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.moduCheck16_17.Location = new System.Drawing.Point(782, 340);
+            this.moduCheck16_17.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
+            this.moduCheck16_17.Name = "moduCheck16_17";
+            this.moduCheck16_17.Size = new System.Drawing.Size(111, 37);
+            this.moduCheck16_17.TabIndex = 23;
+            this.moduCheck16_17.TabStop = false;
+            this.moduCheck16_17.Text = "Modules 16-17";
+            this.moduCheck16_17.UseVisualStyleBackColor = false;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1335, 654);
+            this.Controls.Add(this.moduCheck17);
+            this.Controls.Add(this.moduCheck16);
+            this.Controls.Add(this.moduCheck16_17);
+            this.Controls.Add(this.moduCheck7);
+            this.Controls.Add(this.moduCheck15);
+            this.Controls.Add(this.moduCheck14);
+            this.Controls.Add(this.moduCheck14_15);
+            this.Controls.Add(this.moduCheck13);
+            this.Controls.Add(this.moduCheck12);
+            this.Controls.Add(this.moduCheck11);
+            this.Controls.Add(this.moduCheck11_13);
+            this.Controls.Add(this.moduCheck10);
+            this.Controls.Add(this.moduCheck9);
+            this.Controls.Add(this.moduCheck8);
+            this.Controls.Add(this.moduCheck8_10);
+            this.Controls.Add(this.moduCheck6);
+            this.Controls.Add(this.moduCheck5);
+            this.Controls.Add(this.moduCheck4);
+            this.Controls.Add(this.moduCheck4_7);
             this.Controls.Add(this.moduCheck3);
             this.Controls.Add(this.moduCheck2);
             this.Controls.Add(this.moduCheck1);
@@ -127,5 +385,24 @@
         private System.Windows.Forms.CheckBox moduCheck1;
         private System.Windows.Forms.CheckBox moduCheck2;
         private System.Windows.Forms.CheckBox moduCheck3;
+        private System.Windows.Forms.CheckBox moduCheck6;
+        private System.Windows.Forms.CheckBox moduCheck5;
+        private System.Windows.Forms.CheckBox moduCheck4;
+        private System.Windows.Forms.CheckBox moduCheck4_7;
+        private System.Windows.Forms.CheckBox moduCheck10;
+        private System.Windows.Forms.CheckBox moduCheck9;
+        private System.Windows.Forms.CheckBox moduCheck8;
+        private System.Windows.Forms.CheckBox moduCheck8_10;
+        private System.Windows.Forms.CheckBox moduCheck13;
+        private System.Windows.Forms.CheckBox moduCheck12;
+        private System.Windows.Forms.CheckBox moduCheck11;
+        private System.Windows.Forms.CheckBox moduCheck11_13;
+        private System.Windows.Forms.CheckBox moduCheck15;
+        private System.Windows.Forms.CheckBox moduCheck14;
+        private System.Windows.Forms.CheckBox moduCheck14_15;
+        private System.Windows.Forms.CheckBox moduCheck7;
+        private System.Windows.Forms.CheckBox moduCheck17;
+        private System.Windows.Forms.CheckBox moduCheck16;
+        private System.Windows.Forms.CheckBox moduCheck16_17;
     }
 }
